3a0e5a879f1d83b8b6907c277d106e4c5520fb36
Isel Tula <iseltula04@gmail.com>
2016-06-14 12:45:00 -0400

    Product Detail Finished

--- app/products/product-detail.component.js
+++ app/products/product-detail.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', '../shared/star.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1;
+    var core_1, router_1, star_component_1;
     var ProductDetailComponent;
     return {
         setters:[
@@ -19,6 +19,9 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (star_component_1_1) {
+                star_component_1 = star_component_1_1;
             }],
         execute: function() {
             ProductDetailComponent = (function () {
@@ -27,14 +30,22 @@ System.register(['angular2/core', 'angular2/router'], function(exports_1, contex
                     this._router = _router;
                     this.pageTitle = 'Product Detail';
                     var id = +this._routeParams.get('id');
-                    this.pageTitle += ": " + id;
+                    this.productName = this._routeParams.get('name');
+                    this.productCode = this._routeParams.get('code');
+                    this.releaseDate = this._routeParams.get('date');
+                    this.price = +this._routeParams.get('price');
+                    this.starRating = +this._routeParams.get('starRating');
+                    this.imageUrl = this._routeParams.get('imageUrl');
+                    this.description = this._routeParams.get('description');
+                    this.pageTitle += ": " + this.productName + " ";
                 }
                 ProductDetailComponent.prototype.onBack = function () {
                     this._router.navigate(['Products']);
                 };
                 ProductDetailComponent = __decorate([
                     core_1.Component({
-                        templateUrl: 'app/products/product-detail.component.html'
+                        templateUrl: 'app/products/product-detail.component.html',
+                        directives: [star_component_1.StarComponent]
                     }), 
                     __metadata('design:paramtypes', [router_1.RouteParams, router_1.Router])
                 ], ProductDetailComponent);
--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,16 +1,33 @@
 import { Component } from 'angular2/core';
 import { RouteParams, Router } from 'angular2/router';
+import { IProduct } from './product';
+import { StarComponent } from '../shared/star.component';
 
 @Component({
-    templateUrl: 'app/products/product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
 })
 
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    starRating:number;
+    imageUrl:string;
+    description: string;
 
     constructor(private _routeParams: RouteParams, private _router: Router){
         let id= +this._routeParams.get('id');
-        this.pageTitle += `: ${id}`;
+        this.productName = this._routeParams.get('name');
+        this.productCode = this._routeParams.get('code');
+        this.releaseDate = this._routeParams.get('date');
+        this.price= +this._routeParams.get('price');
+        this.starRating = +this._routeParams.get('starRating');
+        this.imageUrl= this._routeParams.get('imageUrl');
+        this.description= this._routeParams.get('description');
+        this.pageTitle += `: ${this.productName} `;
     }
 
     onBack(): void{
